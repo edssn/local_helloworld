244a077b9c38c6b502a39f3f11dbb9e51c06f1c9
Edisson Fernando Sigua Loja <edissonf.sigua@gmail.com>
2021-04-22 19:37:39 -0500

    Navegacion agregada

---
+++ lib.php
@@ -0,0 +1,75 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die;
+/**
+ * Add link to index.php into navigation drawer.
+ *
+ * @param global_navigation $root Node representing the global navigation tree.
+ */
+function local_helloworld_extend_navigation(global_navigation $navigation) {
+    global $PAGE;
+    if (!$PAGE->course or $PAGE->course->id == 1) {
+        return;
+    }
+
+    $title = get_string('pluginname', 'local_helloworld');
+
+    // Flat navigation
+    $node = navigation_node::create(
+        $title,
+        new moodle_url('/local/helloworld/index.php'),
+        navigation_node::TYPE_CUSTOM,
+        null,
+        null,
+        new pix_icon('t/addcontact', '')
+    );
+    $node->showinflatnavigation = true;
+    $navigation->add_node($node, 'mycourses');
+    $node->add_class('mail_root');
+
+
+    // navigation block
+    $parent = navigation_node::create($title, null, navigation_node::TYPE_COURSE);
+
+    $main_node = $navigation->add_node($parent, 'mycourses');
+    $main_node->nodetype = 1;
+    $main_node->collapse = true;
+    $main_node->forceopen = true;
+    $main_node->isexpandable = true;
+    $main_node->showinflatnavigation = false;
+
+    $course_node = $main_node->add('Tab 1', new moodle_url('/local/helloworld/index.php'));
+    $course_node->set_parent($main_node);
+    $course_node->showinflatnavigation = false;
+
+    $user_node = $main_node->add('Tab 2', new moodle_url('/local/helloworld/index.php'));
+    $user_node->set_parent($main_node);
+    $user_node->showinflatnavigation = false;
+}
+
+function local_helloworld_extend_settings_navigation($settingsnav, $context) {
+    global $CFG, $PAGE;
+
+    // Only add this settings item on non-site course pages.
+    if (!$PAGE->course or $PAGE->course->id == 1) {
+        return;
+    }
+
+    if ($settingnode = $settingsnav->find('courseadmin', navigation_node::TYPE_COURSE)) {
+        $strfoo = get_string('pluginname', 'local_helloworld');
+        $url = new moodle_url('/local/helloworld/index.php', array('id' => $PAGE->course->id));
+        $foonode = navigation_node::create(
+            $strfoo,
+            $url,
+            navigation_node::NODETYPE_LEAF,
+            'myplugin',
+            'myplugin',
+            new pix_icon('t/addcontact', $strfoo)
+        );
+        if ($PAGE->url->compare($url, URL_MATCH_BASE)) {
+            $foonode->make_active();
+        }
+        $settingnode->add_node($foonode);
+    }
+}
+
